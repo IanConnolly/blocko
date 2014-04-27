9025cf65586424849da61445376de3b2440eaa27
Ian Connolly <ian@connolly.io>
2014-04-27 21:16:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+blocker
+=============
+
+for when a RT was an endorsement, block everybody who RT'd a tweet
+
+
+## Usage
+
+```npm install -g blocker```
+
+Create a twitter application and generate an access key.
+
+Stick your API + access keys in config.json
+
+run
+
+```blocker --url="<tweet-url>""```
+
+or
+
+```blocker <tweet-id>```
+
+(warning, will only block the first for the moment)
---
+++ bin/blocker.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+var config = require('../config.json');
+var Twit = require('twit');
+var argv = require('yargs').argv;
+
+
+var in_id;
+
+if (argv.url) {
+    in_id = argv.url.match(/[^/]+$/)[0];
+} else {
+    in_id = argv._[0];
+}
+
+var T = new Twit(config);
+
+function block(id) {
+  T.post('blocks/create', { user_id: id }, function (err, reply) {
+      if (err) console.err(err);
+      if (reply) console.log('Blocked user: ' + reply.id);
+  });
+}
+
+T.get('statuses/retweeters/ids',
+      { id: in_id, count: 100, stringify_ids: true},
+      function(err, reply) {
+          if (err) console.error(err);
+          if (reply) {
+              for (var i = 0; i < reply.ids.length; i++) {
+                  block(reply.ids[i]);
+              }
+          }
+});
